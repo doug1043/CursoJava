9f10271e926e24e80b220b15419add39b828d71f
Douglas Rodrigues <doug104397@gmail.com>
2023-01-23 19:25:38 -0300

    Criação classe GradeBook com atributos

---
+++ src/GradeBook.java
@@ -0,0 +1,16 @@
+public class GradeBook {
+    
+    private String nomeCurso;
+    double[] notasAlunos;
+
+    public String getNomeCurso(){
+        return nomeCurso;
+    }
+
+    public void setNomeCurso(String nomeCurso){
+        this.nomeCurso = nomeCurso;
+    }
+
+
+
+}
